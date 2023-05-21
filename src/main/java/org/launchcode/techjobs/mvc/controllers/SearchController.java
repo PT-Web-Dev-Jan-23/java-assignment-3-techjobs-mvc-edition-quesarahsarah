2397234c2b1a9ae1087220ea760c400d9cacbcbd
Sarah Brown <quesarahsarah90@gmail.com>
2023-05-21 18:00:52 -0500

    completed all tasks

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -31,7 +31,7 @@ public class SearchController {
     @PostMapping(value="results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchType.equals("all")||searchTerm.equals("")){
+        if (searchTerm.equals("all")||searchTerm.equals("")){
             jobs = JobData.findAll();
             model.addAttribute("title", "Jobs with All: " + searchTerm);
         } else {
